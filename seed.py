08e46aa8ff1af525ca2558e7d6786aeccb7213f9
Hackbright Student <info@hackbrightacademy.com>
2015-05-04 17:14:35 -0700

    Completed Judgmental Eye Step 1.

--- seed.py
+++ seed.py
@@ -2,6 +2,7 @@
 
 from model import User, Rating, Movie, connect_to_db, db
 from server import app
+from datetime import datetime
 
 
 def load_users():
@@ -17,16 +18,40 @@ def load_users():
 
     db.session.commit()
 
- 
-
-
-
 def load_movies():
     """Load movies from u.item into database."""
+    filepath = "./seed_data/u.item"
+    movies = open(filepath)
+
+    for movie in movies:
+        movie = movie.rstrip().split('|')
+        title = movie[1][:-6]
+        title = title.decode("latin-1")
+        if movie[2]:
+            date = datetime.strptime(movie[2], '%d-%b-%Y')
+        else:
+            date = None
+        db_movie = Movie(
+            movie_id = movie[0], title = title, 
+            released_at = date, imdb_url = movie[4])
+        db.session.add(db_movie)
+
+    db.session.commit()
 
 
 def load_ratings():
     """Load ratings from u.data into database."""
+    filepath = "./seed_data/u.data"
+    ratings = open(filepath)
+
+    for rating in ratings:
+        rating = rating.rstrip().split()
+
+        db_rating = Rating(movie_id=rating[1], user_id=rating[0],
+            score=rating[2])
+        db.session.add(db_rating)
+
+    db.session.commit()
 
 
 if __name__ == "__main__":
